3439490b707691817632efdd1ddb7864b796f1f1
Abhijit Mondal <abhijit.manpur@gmail.com>
2020-07-08 14:26:22 +0530

    small changes

--- LKM/nova_main.c
+++ LKM/nova_main.c
@@ -20,26 +20,69 @@ MODULE_LICENSE("GPL");
 
 #define LKM_INTERFACE_FILE_PROC "hello"
 
-static char buffer[256] = {0}; static int buffer_len = 0;
+static char redirectionConfigured = 0;
+
+static void configureSyscallRedirection(void) {
+    char *sym_name = "sys_call_table";
+    void **sys_call_table;
+
+    if(redirectionConfigured) return;
+    redirectionConfigured = 1;
+
+    sys_call_table = (void *) kallsyms_lookup_name(sym_name);
+
+    //TODO add a loop
+    NOVA_STORE_ORIG(open, sys_call_table);
+
+    write_cr0(read_cr0() & (~0x10000)); //remove write protection
+
+    //TODO add another loop
+    NOVA_REDIRECT(open, sys_call_table);
+
+    write_cr0(read_cr0() | 0x10000); //restore write protection
+}
+
+static void restorSyscallRedirection(void) {
+    char *sym_name = "sys_call_table";
+    void **sys_call_table;
+
+    if(!redirectionConfigured) return;
+    redirectionConfigured = 0;
+
+    sys_call_table = (void **) kallsyms_lookup_name(sym_name);
+
+    write_cr0(read_cr0() & (~0x10000)); //remove write protection
+
+    //TODO add another loop
+    NOVA_RESTORE(open, sys_call_table);
+
+    write_cr0(read_cr0() | 0x10000); //restore write protection
+}
+// static char buffer[256] = {0}; static int buffer_len = 0;
 
 static ssize_t write(struct file *file, const char *buf, size_t count, loff_t *pos) {
     if(!buf || !count) return -EINVAL;
-    if(copy_from_user(buffer, buf, count < 256 ? count:256)) return -ENOBUFS;
+    if(buf[0]) configureSyscallRedirection();
+    else restorSyscallRedirection();
+    return 1;
 
-    buffer_len = count < 256 ? count:256;
-    printk(KERN_INFO "%.*s", (int)count, buf);
-    return buffer_len;
+//     if(copy_from_user(buffer, buf, count < 256 ? count:256)) return -ENOBUFS;
+//
+//     buffer_len = count < 256 ? count:256;
+//     printk(KERN_INFO "%.*s", (int)count, buf);
+//     return buffer_len;
 }
 
 static ssize_t read(struct file *file, char *buf, size_t count, loff_t *pos) {
-    int ret = buffer_len;
-    if(!buffer_len) return 0;
-    if(!buf || !count) return -EINVAL;
-    if(copy_to_user(buf, buffer, buffer_len)) return -ENOBUFS;
-
-    printk(KERN_INFO "%.*s", (int)buffer_len, buffer);
-    buffer_len = 0;
-    return ret;
+    return -EINVAL;
+//     int ret = buffer_len;
+//     if(!buffer_len) return 0;
+//     if(!buf || !count) return -EINVAL;
+//     if(copy_to_user(buf, buffer, buffer_len)) return -ENOBUFS;
+//
+//     printk(KERN_INFO "%.*s", (int)buffer_len, buffer);
+//     buffer_len = 0;
+//     return ret;
 }
 
 
@@ -70,36 +113,6 @@ static void remove_procfile(void) {
     remove_proc_entry(LKM_INTERFACE_FILE_PROC, NULL);
 }
 
-static void configureSyscallRedirection(void) {
-    char *sym_name = "sys_call_table";
-    void **sys_call_table;
-
-    sys_call_table = (void *) kallsyms_lookup_name(sym_name);
-
-    //TODO add a loop
-    NOVA_STORE_ORIG(open, sys_call_table);
-
-    write_cr0(read_cr0() & (~0x10000)); //remove write protection
-
-    //TODO add another loop
-    NOVA_REDIRECT(open, sys_call_table);
-
-    write_cr0(read_cr0() | 0x10000); //restore write protection
-}
-
-static void restorSyscallRedirection(void) {
-    char *sym_name = "sys_call_table";
-    void **sys_call_table;
-
-    sys_call_table = (void **) kallsyms_lookup_name(sym_name);
-
-    write_cr0(read_cr0() & (~0x10000)); //remove write protection
-
-    //TODO add another loop
-    NOVA_RESTORE(open, sys_call_table);
-
-    write_cr0(read_cr0() | 0x10000); //restore write protection
-}
 
 static int hello_init(void)
 {
@@ -109,7 +122,7 @@ static int hello_init(void)
 
     printk(KERN_ALERT "Hello, world\n");
 
-    configureSyscallRedirection();
+//     configureSyscallRedirection();
 
     return 0;
 }
@@ -118,7 +131,7 @@ static int hello_init(void)
 static void hello_exit(void)
 {
     remove_procfile();
-    restorSyscallRedirection();
+//     restorSyscallRedirection();
     printk(KERN_ALERT "Goodbye, you awesome people\n");
 }
 
