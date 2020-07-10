cd45e160dde2e6ec30f10e50c2f13a12a9c3cf32
Abhijit Mondal <abhijit.manpur@gmail.com>
2020-07-10 22:50:54 +0530

    adding Gateway

---
+++ Gateway/nova_uapi.h
@@ -0,0 +1,1 @@
+../LKM/nova_uapi.h
---
+++ Gateway/nova_userland.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "nova_uapi.h"
+
+static int write_to_file(struct nova_user2lkm *info) {
+    int fd;
+    if(!info) return;
+    fd = open("/proc/" LKM_INTERFACE_FILE_PROC, O_WRONLY);
+    if(!fd) {
+        printf("Cannot open interface file\n");
+        return -1;
+    }
+    int ret = write(fd, info, sizeof(struct nova_user2lkm));
+    close(fd);
+    return ret;
+}
+
+int nova_setpid(pid_t pid) {
+    struct nova_user2lkm info;
+    info.order = NOVA_U2L_SET_PID;
+    info.pid = pid;
+    return write_to_file(&info);
+}
+
+int nova_enable() {
+    struct nova_user2lkm info;
+    info.order = NOVA_U2L_ENABLE;
+    return write_to_file(&info);
+}
+
+int nova_disable() {
+    struct nova_user2lkm info;
+    info.order = NOVA_U2L_ENABLE;
+    return write_to_file(&info);
+}
